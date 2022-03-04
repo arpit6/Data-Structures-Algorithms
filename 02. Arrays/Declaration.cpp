1d3867b507b43fe593dba74d58abcd54e7732094
arpit6 <67603031+arpit6@users.noreply.github.com>
2022-03-04 15:31:33 +0530

    Add files via upload

---
+++ 02. Arrays/Declaration.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[3][3][3] = {{1,2,3,4,5,6,7,8,9}, {10, 11,12,13,14,15,16,17,18},{19,20,21,22,23,24,25,26,27}};
+    int i=0, j=0, k=0;
+    for(int i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            for(k=0; k<3; k++)
+            {
+                cout<<a[i][j][k]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
