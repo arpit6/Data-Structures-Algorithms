2cb15dd6b38fbc305a5edefe50014b020e4a54d5
arpit6 <67603031+arpit6@users.noreply.github.com>
2022-03-04 15:32:35 +0530

    Add files via upload

---
+++ 04. Matrices/03. Sparse Matrix/Sparse_Matrix.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+class Element
+{
+public:
+    int i;
+    int j;
+    int x;
+};
+
+class Sparse
+{
+private:
+    int m;
+    int n;
+    int num;
+    Element *ele;
+
+public:
+    Sparse(int m, int n, int num)
+    {
+        this->m = m;
+        this->n = n;
+        this->num = num;
+        ele = new Element[this->num];
+    }
+    ~Sparse()
+    {
+        delete [] ele;
+    }
+
+    Sparse operator+(Sparse &s);
+    friend istream &operator>>(istream &is, Sparse &s);
+    friend ostream &operator<<(ostream &os, Sparse &s);
+};
+
+Sparse Sparse::operator+(Sparse &s)
+{
+    int i,j,k;
+    if(m!=s.m || n!=s.n)
+        return Sparse(0,0,0);
+
+    Sparse *sum=new Sparse(m,n,num+s.num);
+
+    i=j=k=0;
+    while(i<num && j<s.num)
+    {
+        if(ele[i].i<s.ele[j].i)
+            sum->ele[k++] = ele[i++];
+
+        else if(ele[i].i>s.ele[j].i)
+            sum->ele[k++]=s.ele[j++];
+
+        else
+        {
+            if(ele[i].j<s.ele[j].j)
+                sum->ele[k++]=ele[i++];
+
+            else if(ele[i].j>s.ele[j].j)
+                sum->ele[k++]=s.ele[j++];
+
+            else
+            {
+                sum->ele[k]=ele[i];
+                sum->ele[k++].x=ele[i++].x+s.ele[j++].x;
+            }
+        }
+    }
+
+    for(;i<num;i++)sum->ele[k++]=ele[i];
+    for(;j<s.num;j++)sum->ele[k++]=s.ele[j];
+
+    sum->num=k;
+    return *sum;
+}
+
+istream &operator>>(istream &is, Sparse &s)
+{
+    cout<<"Enter non-zero elements"<<endl;
+    for(int i=0; i<s.num; i++)
+        cin>>s.ele[i].i>>s.ele[i].j>>s.ele[i].x;
+    return is;
+}
+
+ostream &operator<<(ostream &os, Sparse &s)
+{
+    int k=0;
+    for(int i=0; i<s.m; i++)
+    {
+        for(int j=0; j<s.n; j++)
+        {
+            if(s.ele[k].i==i && s.ele[k].j==j)
+                cout<<s.ele[k++].x<<" ";
+            else
+                cout<<"0 ";
+        }
+        cout<<endl;
+    }
+    return os;
+}
+
+
+int main()
+{
+    Sparse S1(5,5,5);
+    Sparse S2(5,5,5);
+    cin>>S1;
+    cin>>S2;
+    Sparse sum = S1+S2;
+    cout<<"First Matrix: "<<endl<<S1;
+    cout<<"Second Matrix: "<<endl<<S2;
+    cout<<"Sum: "<<endl<<sum;
+    return 0;
+}
